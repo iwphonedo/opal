299727f72a1dd4361c2b4bca95d030203707e7a8
ariWeinberg <66802642+ariWeinberg@users.noreply.github.com>
2025-03-13 14:36:56 -0700

    feat: simplify KafkaUIContainer implementation and enhance environment variable configuration

--- tests/containers/kafka/kafka_ui_container.py
+++ tests/containers/kafka/kafka_ui_container.py
@@ -1,45 +1,21 @@
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.network import Network
 
-from tests.containers.kafka.kafka_broadcast_container import KafkaBroadcastContainer
-from tests.containers.permitContainer import PermitContainer
-from tests.containers.kafka.settings.kafka_broadcast_settings import KafkaBroadcastSettings
 
-
-class KafkaUIContainer(PermitContainer, DockerContainer):
-    def __init__(
-        self,
-        network: Network,
-        kafka_container: KafkaBroadcastContainer,
-        kafka_settings: KafkaBroadcastSettings,
-        docker_client_kw: dict | None = None,
-        **kwargs,
-    ) -> None:
-        # Add custom labels to the kwargs
-        labels = kwargs.get("labels", {})
-        labels.update({"com.docker.compose.project": "pytest"})
-        kwargs["labels"] = labels
-
-        self.settings = kafka_settings
-
-        self.kafka_container = kafka_container
-        self.network = network
-
-        self.image = "provectuslabs/kafka-ui:latest"
-
-        PermitContainer.__init__(self)
-        DockerContainer.__init__(
-            self, image=self.image, docker_client_kw=docker_client_kw, **kwargs
-        )
-
-
-        for key, value in self.settings.getKafkaUiEnvVars().items():
+class KafkaUIContainer(DockerContainer):
+    def __init__(self, network: Network):
+        self.settings = {
+            "env": {
+                "KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS": "kafka0:9092",
+            }
+        }
+        DockerContainer.__init__(self, image="provectuslabs/kafka-ui:latest")
+        self.with_exposed_ports(8080)
+        for key, value in self.settings.get("env").items():
             self.with_env(key, value)
-
-        self.with_bind_ports(8080, self.settings.kafka_ui_port)
-        self.with_name(self.settings.kafka_ui_container_name)
-
-        self.with_network(self.network)
-        self.with_network_aliases("Kafka_ui")
+        self.with_name("kafka-ui")
+        self.with_network(network)
+        self.with_network_aliases("kafka-ui")
 
         self.start()
+     
