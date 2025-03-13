fdb97f6dfa684ec35010fc397f115f5e37e16212
ariWeinberg <66802642+ariWeinberg@users.noreply.github.com>
2025-03-13 15:55:22 -0700

    feat: refactor ZookeeperContainer to simplify initialization and enhance environment configuration

--- tests/containers/kafka/zookeeper_container.py
+++ tests/containers/kafka/zookeeper_container.py
@@ -1,44 +1,24 @@
-import debugpy
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.network import Network
 
-import docker
-from tests.containers.permitContainer import PermitContainer
-from tests.containers.kafka.settings.kafka_broadcast_settings import KafkaBroadcastSettings
+class ZookeeperContainer(DockerContainer):
+    def __init__(self, network: Network):
+        
+        self.settings = {
+            "env": {
+                "ZOOKEEPER_CLIENT_PORT": "2181",
+                "ZOOKEEPER_TICK_TIME": "2000",
+                "ALLOW_ANONYMOUS_LOGIN": "yes",
+            }
+        }
 
-
-class ZookeeperContainer(PermitContainer, DockerContainer):
-    def __init__(
-        self,
-        network: Network,
-        settings: KafkaBroadcastSettings,
-        docker_client_kw: dict | None = None,
-        **kwargs: dict,
-    ) -> None:
-        # Add custom labels to the kwargs
-        labels = kwargs.get("labels", {})
-        labels.update({"com.docker.compose.project": "pytest"})
-        kwargs["labels"] = labels
-
-        self.network = network
-
-        PermitContainer.__init__(self)
-        DockerContainer.__init__(
-            self,
-            image="confluentinc/cp-zookeeper:latest",
-            docker_client_kw=docker_client_kw,
-            **kwargs,
-        )
-
-        self.with_bind_ports(2181, 2181)
-        self.with_env("ZOOKEEPER_CLIENT_PORT", "2181")
-        self.with_env("ZOOKEEPER_TICK_TIME", "2000")
-        self.with_env("ALLOW_ANONYMOUS_LOGIN", "yes")
-
-        self.with_network(self.network)
-
-        self.with_network_aliases("zookeper")
-        # Add a custom name for the container
-        self.with_name(f"zookeeper")
+        DockerContainer.__init__(self, image="confluentinc/cp-zookeeper:6.2.0")
+        self.with_exposed_ports(2181)
+        
+        self.with_name("zookeeper")
+        for key, value in self.settings.get("env").items():
+            self.with_env(key, value)
+        self.with_network(network)
+        self.with_network_aliases("zookeeper")
 
         self.start()
